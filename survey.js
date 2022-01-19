ec3eaabcaa900e5fbbb803a87f46550f0fdcb74a
ShayFae <ifrahnoor108@gmail.com>
2022-01-19 04:03:17 +0000

    Typo

--- survey.js
+++ survey.js
@@ -5,9 +5,9 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What's your name? We also except nicknames. ", (answer) => {
+rl.question("What's your name? We also except nicknames.", (answer) => {
   let one = answer;
-  rl.question("What's your favourite season. ", (answer1) => {
+  rl.question("What's your favourite season.", (answer1) => {
     let two = answer1;
     rl.question("What music do you listen to?", (answer2) => {
       let three = answer2;
@@ -15,10 +15,10 @@ rl.question("What's your name? We also except nicknames. ", (answer) => {
         let four = answer3;
         rl.question("What's your favourite food?", (answer4) => {
           let five = answer4;
-          rl.question("What's your favourite sport.", (answer6) => {
-            let six = answer6;
-            rl.question("What is your superpower?", (answer7) => {
-              let seven = answer7;
+          rl.question("What's your favourite sport.", (answer5) => {
+            let six = answer5;
+            rl.question("What is your superpower?", (answer6) => {
+              let seven = answer6;
               console.log(`${one}'s favourite season is ${two}, they love to jam to ${three}. they are super into ${four}, and love to eat ${five}. they enjoy ${six} and if they could have any superpower it would be ${seven}`);
               rl.close();
             });
